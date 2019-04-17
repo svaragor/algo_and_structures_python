6f7b8cc040ebfb9cc44741f456a3cd8120578bbe
svaragor <svaragor@gmail.com>
2019-04-17 14:01:24 +0300

    Добавил рекурсию

--- Lesson_2/3.py
+++ Lesson_2/3.py
@@ -4,9 +4,21 @@
  то надо вывести число 6843.
 """
 a = int(input ('введите натуральное число '))
+b = a
 switch_num = ''
 while a>0:
 	digit = a%10
 	a=a//10
 	switch_num = switch_num + str(digit) 
 print(switch_num)
+
+#Рекурсия
+def switchNum (x, x_str):
+	if x==0:
+		print(x_str)
+		return
+	digit = x%10
+	x_str = x_str + str(digit)
+	switchNum(x//10, x_str)
+
+switchNum(b, '')
