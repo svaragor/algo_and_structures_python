9c63c37d841ffa6b066e604c60d566e731bd7124
Svaragor <svaragor@gmail.com>
2019-04-20 12:51:19 +0300

    все

--- Lesson_3/1.py
+++ Lesson_3/1.py
@@ -1,2 +1,17 @@
 # 1.	В диапазоне натуральных чисел от 2 до 99 определить,
 # сколько из них кратны каждому из чисел в диапазоне от 2 до 9.
+
+def kratnost (x, y):
+    if x%y == 0:
+        return True
+    else:
+        return False
+
+a = [0 for i in range(2, 10)]
+b = [j for j in range(2, 10)]
+for i in range (2, 100):
+    for j in range (0, len(a)):
+        if kratnost(i, j+2):
+            a[j] = a[j] +1
+for i in range(0, len(a)):
+    print ('кратных {} -  вдиапазоне (2-99) {} чисел'.format(b[i], a[i]))
